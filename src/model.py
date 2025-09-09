e15068fb907a58d3d2045a49dc4e53bae3ec6102
natanrizzo <natan.rizzo22@gmail.com>
2025-09-09 10:51:39 -0300

    feat(image): image creation still bugs

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 from src.nodes.train_kmeans__node import TrainKMeans
 from src.nodes.pixel_formater__node import PixelFormater
 from PIL import Image
+import numpy as np
 
 class Model:
     def __init__(self):
@@ -36,6 +37,11 @@ class Model:
 
         pixel_formatter = PixelFormater()
         # Run image.
+        
+        img_shape = (height, width, 3)
+
+        background=np.zeros(shape=img_shape, dtype=np.uint8)
+        object=np.zeros(shape=img_shape, dtype=np.uint8)
 
         y=0
         while y < height:
@@ -43,8 +49,16 @@ class Model:
             while x < width:
                 form_pixel = pixel_formatter.run({ 'pixel_rgb': pixels[x, y] }),
                 group = kmeans.predict(form_pixel)
-
-                print(group)
-
+                if (group == 0): # Background
+                    background[x, y] = pixels[x, y]
+                    object[x, y] = (0, 0, 0)
+                else: # Object
+                    background[x, y] = (0, 0, 0)
+                    object[x, y] = pixels[x, y]
                 x+=1
             y+=1
+        
+        background_image = Image.fromarray(background)
+        object_image = Image.fromarray(object)
+        background_image.save("output", "background.jpeg")
+        object_image.save("output", "object.jpeg")
